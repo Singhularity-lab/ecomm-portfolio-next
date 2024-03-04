bce9ab3e0f4de1b4b27c04992d791dc8eccd988c
prabhjeevnijjar <prabhjeevnijjar@gmail.com>
2024-03-04 15:55:28 +0530

    sale component done

--- app/page.js
+++ app/page.js
@@ -1,16 +1,18 @@
 import Banner from './components/HomeComp/Banner';
 import Category from './components/HomeComp/Category';
 import NewArrival from './components/HomeComp/NewArrival';
+import Sale from './components/HomeComp/Sale';
 import Support from './components/HomeComp/Support';
 
 const HomePage = () => {
   return (
-    <div>
+    <>
       <Banner />
       <Category />
       <NewArrival />
       <Support />
-    </div>
+      <Sale />
+    </>
   );
 };
 
